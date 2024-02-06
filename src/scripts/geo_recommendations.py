d16dfc856f74fceabf31b973751a42bb77ffc3c0
s-sushko <ssushko.aw@gmail.com>
2024-02-06 04:34:13 +0300

    Update geo_recommendations.py

--- src/scripts/geo_recommendations.py
+++ src/scripts/geo_recommendations.py
@@ -46,24 +46,34 @@ def main():
 
     geo_events = gu.get_events(spark, basePath , paths) #Читаем events из prod
 
-    geo_users_full_deduplicated = gtu.calculate_user_pairs_by_conditions(geo_events)   #Рассчитываем DataFrame уникальных пар юзеров без сообщений и с общими каналами
+    geo_users_full_deduplicated = gtu.calculate_user_pairs_by_conditions(geo_events).checkpoint()   #Рассчитываем DataFrame уникальных пар юзеров без сообщений и с общими каналами
 
     geo_min_distance = gtu.calculate_events_nearest_city(geo_events, geo_cities) #Рассчитываем город последнего события пользователя
     
 
     #Соединяем уникальные пару юзеров c последним событием для каждого пользователя из пары, находим пары из одного города, выбираем самый свежий datetime из пары, добавляем localtime
-    geo_recommendations = geo_users_full_deduplicated\
+    geo_recommendations_filtered = geo_users_full_deduplicated\
         .join(geo_min_distance,geo_users_full_deduplicated.user_id_1==geo_min_distance.user_id,how='inner')\
-        .selectExpr('user_id_1','user_id_2','datetime as datetime_1','zone_id as zone_id_1')\
+        .selectExpr('user_id_1','user_id_2','datetime as datetime_1','zone_id as zone_id_1',
+                    'event_lat_rad as event_lat_rad_1','event_lon_rad as event_lon_rad_1')\
         .join(geo_min_distance,geo_users_full_deduplicated.user_id_2==geo_min_distance.user_id,how='inner')\
-        .selectExpr('user_id_1','user_id_2','datetime_1','zone_id_1','datetime as datetime_2', 'zone_id as zone_id_2')\
-        .filter(F.col('zone_id_1')==F.col('zone_id_2'))\
+        .selectExpr('user_id_1','user_id_2','datetime_1','zone_id_1','datetime as datetime_2', 'zone_id as zone_id_2',
+                    'event_lat_rad_1','event_lon_rad_1','event_lat_rad as event_lat_rad_2','event_lon_rad as event_lon_rad_2')\
+        .withColumn("event_sphere_distance",\
+                        2*6371*F.asin(
+                        (F.sqrt((F.pow(F.sin((F.col('event_lat_rad_1') - F.col('event_lat_rad_2'))/F.lit(2)),2))\
+                        +F.cos('event_lat_rad_1')*F.cos('event_lat_rad_2')\
+                        *(F.pow(F.sin((F.col('event_lon_rad_1') - F.col('event_lon_rad_2'))/F.lit(2)),2))                            
+                        ))))\
+        .filter(F.col('event_sphere_distance'<=1))
+    
+    geo_recommendations_full = geo_recommendations_filtered\
         .withColumn('processed_dttm',F.greatest("datetime_1","datetime_2"))\
         .join(geo_cities_tz,F.col('zone_id_1')==geo_cities_tz.id,'inner')\
         .withColumn('local_time',F.from_utc_timestamp(F.col('processed_dttm'),F.col('timezone')))\
-        .selectExpr('user_id_1 as user_left','user_id_2 as user_right', 'processed_dttm', 'zone_id_1 as zone_id','local_time') 
+        .selectExpr('user_id_1 as user_left','user_id_2 as user_right', 'processed_dttm', 'zone_id_1 as zone_id','local_time')
 
-    geo_recommendations.write.mode("overwrite").parquet(f"{output_base_path}date={date}") # Записываем результат в analytics
+    geo_recommendations_full.write.mode("overwrite").parquet(f"{output_base_path}date={date}") # Записываем результат в analytics
 
 
 
