4bf8e9e550df0fa3bfd7809a9979d0ccf8630253
s-sushko <ssushko.aw@gmail.com>
2024-02-06 04:34:20 +0300

    Update geo_user_activity.py

--- src/scripts/geo_user_activity.py
+++ src/scripts/geo_user_activity.py
@@ -58,7 +58,7 @@ def main():
     #Считаем домашний город как самую свежую последовательность events >= 27 дней
     #Берем lag с текущей строкой по городу, ставим ноль при совпадении, размечаем напрерывные последовательности(новая последовательность начинается, если в is_prev_city появилась 1)
     #Считаем длину последовательности 
-    prev_city_window = Window.Window().partitionBy('user_id').orderBy(F.desc('date'))
+    prev_city_window = Window.Window().partitionBy('user_id').orderBy(F.desc('date')) 
     city_seq_window = Window.Window().partitionBy('user_id').orderBy(F.desc('date')).rowsBetween(Window.Window.unboundedPreceding, 0)
     geo_home_city = geo_min_distance\
                     .withColumn("is_prev_city",F.when(F.col('city')==F.lag('city').over(prev_city_window),0).otherwise(1))\
@@ -68,14 +68,31 @@ def main():
                     .filter(F.col('seq_long')>=27)\
                     .groupBy(['user_id','city']).agg(F.min('seq_group'))\
                     .selectExpr('user_id','city as home_city')
+    
+    #                Ответ на комментарий из ревью - Выбор последовательности с самой новой датой заложен в значение seq_group. 
+    #                Подробнее по строкам:
+    #                prev_city_window = Window.Window().partitionBy('user_id').orderBy(F.desc('date'))
+    #                .withColumn("is_prev_city",F.when(F.col('city')==F.lag('city').over(prev_city_window),0).otherwise(1))\ 
+    #                1 присваиваем только 1 строке в последовательности(city != lag(city)), остальным строкам - 0
 
-    #Считаем кол-во путешествий
-    geo_travel_count = geo_min_distance.groupBy('user_id','date','city').agg(F.expr('count(*) as row_count'))\
-        .groupBy('user_id').agg(F.expr('count(city) as travel_count')).orderBy('user_id')
+    #                .withColumn("seq_group",F.sum('is_prev_city').over(city_seq_window))\ # Суммируем в рамках последовательности значения "is_prev_city". 
+    #                По сути работает как ранжирование, т.е. все строкам в рамках одной последовательности(от 1 до 1 в is_prev_city, с помощью форточки) присваевается одно значение, 
+    #                которое инкрементируется на 1 в след. последовательности
+    #                Т.к. окна prev_city_window и city_seq_window имеют order by date desc - последовательности ранжируются от самой новой к самой старой
 
-    #Считаем уникальные посещенные города
-    geo_travel_array = geo_min_distance.groupBy('user_id','date','city').agg(F.expr('count(*) as row_count'))\
-        .groupBy('user_id').agg(F.collect_set('city').alias('travel_array'))
+    #                .groupBy(['user_id','city','seq_group']).agg(F.expr('count(seq_group) as seq_long'))\ группируем по номеру последовательности и считаем кол-во строк в ней
+
+    #                .groupBy(['user_id','city']).agg(F.min('seq_group'))\ # Здесь мы просто берем минимальный номер последовательности, т.е. последовательность с самой новой датой.
+  
+    #Считаем уникальные посещенные города и кол-во путешествий
+    travel_array_window = Window.Window().partitionBy('user_id').orderBy(F.asc('date'))
+
+    geo_travel_array = geo_min_distance.select('user_id','date','city')\
+        .withColumn('is_prev_city', F.when(F.col('city')==F.lag('city').over(travel_array_window),0).otherwise(1))\
+        .filter(F.col('is_prev_city')==1)\
+        .groupBy('user_id').agg(F.collect_list('city').alias('travel_array'))\
+        .withColumn('travel_count', F.size('travel_array'))\
+        .select('user_id','travel_count','travel_array')
 
     #Считаем local time
     geo_local_time = geo_min_distance.filter(F.col('datetime').isNotNull())\
@@ -85,8 +102,8 @@ def main():
         .select('user_id','local_time')
 
     #Соединяем в витрину
-    geo_user_activity = geo_act_city.join(geo_home_city,['user_id'],how='left')\
-        .join(geo_travel_count,['user_id'],how='left')\
+    geo_user_activity = geo_act_city\
+        .join(geo_home_city,['user_id'],how='left')\
         .join(geo_travel_array,['user_id'],how='left')\
         .join(geo_local_time,['user_id'],how='left')
 
